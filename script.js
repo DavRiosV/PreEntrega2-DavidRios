84ea31312463abccb6562a47fb5b81540cd6f1a5
David Rios <david.r.v@hotmail.com>
2022-12-10 23:23:23 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,122 @@
+const pokemonPlush = [
+    {
+        "codigo": "0001",
+        "producto": "Peluche Pikachu",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "file:///C:/Users/David/Downloads/Javascript/Mi-preentrega2David-Rios/IMG/pikachu.png",
+        "oferta": false
+    },{
+        "codigo": "0002",
+        "producto": "Peluche Pikachu durmiendo",
+        "precio": "$20.000 CLP",
+        "descripcion": "Peluche de 40 cm de alto",
+        "img": "https://cdn.imgbin.com/10/19/15/imgbin-pok-mon-x-and-y-pikachu-pok-mon-universe-plush-pikachu-hqdqjSguc9F6W45SR7xBetqgu.jpg",
+        "oferta": false
+    },{
+        "codigo": "0003",
+        "producto": "Peluche Pikachu pareja",
+        "precio": "$20.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://w7.pngwing.com/pngs/800/685/png-transparent-pikachu-stuffed-animals-cuddly-toys-centre-pokemon-plush-pikachu-baby-toys-material-doll.png",
+        "oferta": true
+    },{
+        "codigo": "0004",
+        "producto": "Peluche Charmander",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://e7.pngegg.com/pngimages/783/469/png-clipart-plush-pokemon-red-and-blue-pikachu-stuffed-animals-cuddly-toys-charmander-pikachu-orange-textile.png",
+        "oferta": false
+    },{
+        "codigo": "0005",
+        "producto": "Peluche Charmeleon",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i5.walmartimages.com/asr/ee070512-d66d-4ee4-ba08-fa746453a194_1.d2b76fd5ee2a516f3f230aa37a35c35d.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFFg",
+        "oferta": false
+    },{
+        "codigo": "0006",
+        "producto": "Peluche Charizard",
+        "precio": "$20.000 CLP",
+        "descripcion": "Peluche de 40 cm de alto",
+        "img": "https://www.pngfind.com/pngs/m/33-337247_1-of-pokemon-charizard-plush-hd-png-download.png",
+        "oferta": false
+    },{
+        "codigo": "0007",
+        "producto": "Peluche Holiday Bulbasaur",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i.ebayimg.com/images/g/RRUAAOSwLwNhdeY3/s-l500.png",
+        "oferta": false
+    },{
+        "codigo": "0008",
+        "producto": "Peluche Ivysaur",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://cdn.shopify.com/s/files/1/0328/8888/1292/products/2020-12-29_12.10.18_1024x1024@2x.png?v=1609229705",
+        "oferta": false
+    },{
+        "codigo": "0009",
+        "producto": "Peluche Venasaur",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i.ebayimg.com/images/g/U~EAAOSwOhtfWucZ/s-l500.png",
+        "oferta": false
+    },{
+        "codigo": "0010",
+        "producto": "Peluche Squirtle",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i.ebayimg.com/images/g/tK0AAOSwSxNjWaVG/s-l500.png",
+        "oferta": false
+    },{
+        "codigo": "0011",
+        "producto": "Peluche Wartortle",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i5.walmartimages.com/asr/3d97ad43-ea26-4317-a4a1-860c617516e6_1.b0356d3ab2a9a711a825ad4ed4494aae.jpeg",
+        "oferta": false
+    },{
+        "codigo": "0012",
+        "producto": "Peluche Blastoise",
+        "precio": "$15.000 CLP",
+        "descripcion": "Peluche de 30 cm de alto",
+        "img": "https://i.ebayimg.com/images/g/tuwAAOSwg11h9Xvx/s-l500.png",
+        "oferta": false
+    },{
+        "codigo": "0013",
+        "producto": "Peluche Ditto Bulbasaur",
+        "precio": "$12.000 CLP",
+        "descripcion": "Peluche de 20 cm de alto",
+        "img": "https://cdn.imgbin.com/2/25/8/imgbin-pikachu-bulbasaur-stuffed-animals-cuddly-toys-ditto-plush-pikachu-yviSJyJNuGQGYk9NHCSMiCLzT.jpg",
+        "oferta": true
+    },{
+        "codigo": "0014",
+        "producto": "Peluche Ditto Charmander",
+        "precio": "$12.000 CLP",
+        "descripcion": "Peluche de 20 cm de alto",
+        "img": "https://www.asakura-japan.com/data/asakura-japan/product/20160328_f609fa.jpg",
+        "oferta": true
+    },{
+        "codigo": "0015",
+        "producto": "Peluche Ditto Squirtle",
+        "precio": "$12.000 CLP",
+        "descripcion": "Peluche de 20 cm de alto",
+        "img": "https://www.asakura-japan.com/data/asakura-japan/product/20160328_f609fa.jpg",
+        "oferta": true
+    },{
+        "codigo": "0016",
+        "producto": "Peluche Weird Pikachu",
+        "precio": "$20.000 CLP",
+        "descripcion": "Peluche de 35 cm de alto",
+        "img": "https://www.asakura-japan.com/data/asakura-japan/product/20160328_f609fa.jpg",
+        "oferta": false
+    },{
+        "codigo": "0017",
+        "producto": "Peluche Weird Pikachu",
+        "precio": "$20.000 CLP",
+        "descripcion": "Peluche de 35 cm de alto",
+        "img": "https://i.ebayimg.com/images/g/HggAAOSw~FxfOik6/s-l1600.png",
+        "oferta": false
+    }
+]
