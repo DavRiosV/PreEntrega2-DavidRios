04301e93f6738e37878e6e2abd1bd6c3f3c36181
David Rios <david.r.v@hotmail.com>
2022-12-13 23:30:12 -0300

    nuevo commit 13/12

--- index.js
+++ index.js
@@ -12,42 +12,71 @@ function cardsHtml ( array ) {
             <h2>
                 ${pokemonPlush.producto}
             </h2>
-            <button> Comprar</button>
+            <p>
+            ${pokemonPlush.precio}
+            </p>
+            <button>Comprar</button>
         `
         contenedor.appendChild(card)
     })    
 }
 
+
 cardsHtml(pokemonPlush)
 
-function pokemonascendente (pokemonPlush) {
-
-    
     const pokemonascendente = [...pokemonPlush].sort((a, b) => {
-        if ( a < b ){
-            return -1
-        } else if ( a > b){
-            return 1
-        } else {
-            return 0
-        }
+        return parseInt (b.id) - parseInt (a.id)
     })
     
     console.log(pokemonPlush)
     console.log(pokemonascendente)
     
     const pokemonDescendente = [...pokemonPlush].sort((a, b) => {
-        if ( a < b ){
-            return 1
-        } else if ( a > b){
-            return -1
-        } else {
-            return 0
-        }
+        return parseInt (a.id) - parseInt (b.id)
     })
-    
     console.log(pokemonDescendente)
+
+    function ascendente (propiedad){
+        const nombrePokemonA = [...pokemonPlush].sort((a, b) => {
+            if(a[propiedad].toLocaleLowerCase() < b[propiedad].toLocaleLowerCase()){
+                return 1;
+            } else if (a[propiedad].toLocaleLowerCase() > b[propiedad].toLocaleLowerCase()){
+                return -1;
+            } 
+            return 0;
+        })
+        console.log(nombrePokemonA)
+    }
+
+    function descentende (propiedad){
+        const nombrePokemonZ = [...pokemonPlush].sort((a, b) => {
+            if(a[propiedad].toLocaleLowerCase() > b[propiedad].toLocaleLowerCase()){
+                return 1;
+            } else if (a[propiedad].toLocaleLowerCase() < b[propiedad].toLocaleLowerCase()){
+                return -1;
+            } 
+            return 0;
+        })
     
-}
-    
-    confirm("Bienvenidos a R&A ¿Quieres ver nuestras ofertas?")
+        console.log(nombrePokemonZ)
+    }
+
+
+
+        var confirma = confirm("Bienvenidos a R&A ¿Quieres ver nuestras ofertas?")
+        console.log(confirma)
+        if ( confirma) {
+            const contenedor = document.querySelector(".container").innerHTML = " "; 
+            cardsHtml(pokemonPlush.filter(item => item.oferta));
+        }
+
+        
+    function orden (){
+        const valor = document.getElementById("ordenar").value;
+        if ("value1" == valor) {
+            descentende ("producto")
+        } else { ("value2" == valor) 
+            ascendente ("producto")
+        }
+    }
+
